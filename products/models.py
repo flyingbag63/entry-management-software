295395e80df0572487b639f35cb3d4173b0ba411
flyingbag63 <bhaskarmd44@gmail.com>
2019-12-01 00:51:36 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -9,5 +9,5 @@ class Entry(models.Model):
 	check_in_time   = models.TimeField()
 	check_out_time  = models.TimeField()
 	host_name       = models.CharField(max_length = 120) 
-	host_email 		= models.EmailField(max_length = 254)
+	host_email 	= models.EmailField(max_length = 254)
 	host_phone      = models.CharField(max_length = 10)
